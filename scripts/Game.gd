0814268a36e79fb3ccd0529e2e4558afcbeb3968
Alejandro Vidal <alejandro.vidal.castillo@gmail.com>
2021-01-30 17:43:24 -0300

    Agregado comportamiento de tomar y dejar esencias

---
+++ scripts/Game.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var personaje
+var pantallas
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+const SIN_ESENCIA: int = -1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	personaje = $Personaje
+	pantallas = [$Pantalla1, $Pantalla2, $Pantalla3]
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _input(event):
+	if event.is_action_released('ui_accept'):
+		var esencia = personaje.get_esencia()
+		# qué pantalla estamos, para que nos dé su esencia
+		var pantalla = personaje.get_pantalla()
+		if esencia == SIN_ESENCIA: # obtiene una esencia
+			var esencia_de_la_pantalla_actual = pantallas[pantalla].get_esencia()
+			personaje.set_esencia(esencia_de_la_pantalla_actual)
+		else :
+			var result = pantallas[pantalla].give_esencia(esencia) 
+			print(result)
+			personaje.set_esencia(SIN_ESENCIA)
--- scripts/Personaje.gd
+++ scripts/Personaje.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-var pantalla_actual = 0
+var pantalla_actual = 1
 var pantallas = [0, 1, 2]
 var posiciones_pantallas = []
+export var esencia_actual = -1
+
+# sprite = "asas_" + esencia_actual + "_.png"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,16 +19,25 @@ func _ready():
 	root.get_node("Positions/PosicionPantalla3")]
 	position = posiciones_pantallas[1].position
 
-func goto_pantalla(index):
-	position = posiciones_pantallas[index].position
-	pantalla_actual = index
-	#$Planet2sfx.stop()
-	#$Planet1sfx.stop()
-	#$Planet0sfx.play()
-	
 func _input(event):
 	if event.is_action_released('ui_right') and pantalla_actual != 2:
 		goto_pantalla( (pantalla_actual + 1) )
 			
 	if event.is_action_released('ui_left')  and pantalla_actual != 0:
 		goto_pantalla( (pantalla_actual - 1) )
+
+func goto_pantalla(index):
+	position = posiciones_pantallas[index].position
+	pantalla_actual = index
+	#$Planet2sfx.stop()
+	#$Planet1sfx.stop()
+	#$Planet0sfx.play()
+
+func get_esencia():
+	return esencia_actual
+	
+func set_esencia(esencia):
+	esencia_actual = esencia
+	
+func get_pantalla():
+	return pantalla_actual
